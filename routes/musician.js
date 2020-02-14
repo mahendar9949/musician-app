af18214fe3cbb9ba9fc860cf957f57a1e08a087b
Mahendar Reddy Baddam <MB0C79107@techmahindra.com>
2020-02-14 15:33:59 +0530

    second

--- routes/musician.js
+++ routes/musician.js
@@ -8,6 +8,10 @@ const schema = require('../store/schema');
 router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
+
+
+
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
